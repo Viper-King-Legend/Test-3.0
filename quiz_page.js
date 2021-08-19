bfcf083467e0bb943f8bedb924d8f38d790ab3d2
Viper-King-Legend <78768501+Viper-King-Legend@users.noreply.github.com>
2021-08-19 10:20:00 -0400

    Add files via upload

--- quiz_page.js
+++ quiz_page.js
@@ -20,9 +20,9 @@ function send(){
     actual_answer = parseInt(number1) * parseInt(number2);
     question_number = "<h4>" + number1 + " X " + number2 + " </h4> ";
     input_box = "<br>Answer : <input type = 'text' id='input_check_box'>";
-    check_button = "<br><br><button class = 'btn btn-info> onclick = 'check()'>Check</button>";
+    check_button = "<br><br><button class = 'btn btn-success> onclick = 'check()'>Check</button>";
     row = question_number + input_box + check_button;
-    document.getElementById("ouput").innerHTML = row;
+    document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 
